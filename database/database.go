ed1724b0dceb422e44c939d2d71a14d291f87d02
Marius Biegger <biegger.marius@gmail.com>
2024-10-16 00:25:34 +0200

    impl pg adapter

---
+++ database/database.go
@@ -0,0 +1,96 @@
+package database
+
+import (
+    "database/sql"
+    "fmt"
+    "github.com/BieggerM/userservice/models"
+    _ "github.com/lib/pq"
+)
+
+type Database interface {
+    SaveUser(user models.User)
+    DeleteUser(username string)
+    UpdateUser(user models.User)
+    GetUser(username string) models.User
+}
+
+// Postgres is the PostgreSQL database connection
+type Postgres struct {
+    DB *sql.DB
+}
+
+// Connect connects to the PostgreSQL database
+func (p *Postgres) connect() {
+    var err error
+    p.DB, err = sql.Open("postgres", "user=postgres dbname=postgres sslmode=disable password=postgres")  
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+// SaveUser saves a user to the PostgreSQL database
+func (p *Postgres) SaveUser(user models.User) models.User {
+    p.connect()
+    defer p.DB.Close()
+    _, err := p.DB.Exec("insert into users (username, firstname, lastname) values ($1, $2, $3)", user.Username, user.FirstName, user.LastName)
+    if err != nil {
+        fmt.Println(err)
+    }
+    return user
+}
+
+// DeleteUser deletes a user from the PostgreSQL database
+func (p *Postgres) DeleteUser(username string) {
+    p.connect()
+    defer p.DB.Close()
+    _, err := p.DB.Exec("delete from users where username = $1", username)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+// UpdateUser updates a user in the PostgreSQL database
+func (p *Postgres) UpdateUser(user models.User) models.User {
+    p.connect()
+    defer p.DB.Close()
+    _, err := p.DB.Exec("update users set firstname = $1, lastname = $2 where username = $3", user.FirstName, user.LastName, user.Username)
+    if err != nil {
+        fmt.Println(err)
+    }
+    return user
+}
+
+// GetUser gets a user from the PostgreSQL database
+func (p *Postgres) GetUser(username string) models.User {
+    p.connect()
+    defer p.DB.Close()
+
+    user := models.User{}
+    err := p.DB.QueryRow("select username, firstname, lastname from users where username = $1", username).Scan(&user.Username, &user.FirstName, &user.LastName)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            fmt.Println("No user found with the given username")
+        } else {
+            fmt.Println(err)
+        }
+    }
+    return user
+}
+
+// ListUsers lists all users from the PostgreSQL database
+func (p *Postgres) ListUsers() []models.User {
+    p.connect()
+    defer p.DB.Close()
+    rows, err := p.DB.Query("select * from users")
+    if err != nil {
+        fmt.Println(err)
+    }
+    users := []models.User{}
+    for rows.Next() {
+        user := models.User{}
+        rows.Scan(&user.Username, &user.FirstName, &user.LastName)
+        users = append(users, user)
+    }
+    return users
+}
+
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/BieggerM/userservice/database"
+	"github.com/BieggerM/userservice/models"
+)
+
+var DB database.Postgres
+
+func main() {
+	// create database implementation
+	r := gin.Default()
+	r.GET("/users", listUsers)
+	r.GET("/users/:username", getUser)
+	r.POST("/users", createUser)
+	r.PATCH("/users", updateUser)
+	r.DELETE("/users/", deleteUser)
+	r.Run(":8080")
+}
+
+func listUsers(c *gin.Context) {
+	users := DB.ListUsers()
+	c.JSON(200, gin.H{
+		"users": users,
+	})
+
+}
+
+func getUser(c *gin.Context) {
+	user := DB.GetUser(c.Param("username"))
+	c.JSON(200, gin.H{
+		"username" : user.Username,
+		"firstname" : user.FirstName,
+		"lastname" : user.LastName,
+	})
+
+}
+
+func createUser(c *gin.Context) {
+	var user models.User
+	c.ShouldBindBodyWithJSON(&user)
+	DB.SaveUser(user)
+	c.JSON(200, gin.H{
+		"message": "user created",
+	})
+}
+
+func updateUser(c *gin.Context) {
+	var user models.User
+	c.ShouldBindBodyWithJSON(&user)
+	DB.UpdateUser(user)
+	c.JSON(200, gin.H{
+		"message": "user updated",
+		"username" : user.Username,
+		"firstname" : user.FirstName,
+		"lastname" : user.LastName,
+	})
+}
+
+func deleteUser(c *gin.Context) {
+	var user models.User
+	c.ShouldBindBodyWithJSON(&user)
+	DB.DeleteUser(user.Username)
+	c.JSON(200, gin.H{
+		"message": "user deleted",
+		"username" : user.Username,
+	})
+}
